8ff44c404d23f9ba38c89ab12be0ce7b9b5b79da
vectorg3 <diglytail@gmail.com>
2023-02-28 09:27:24 +0300

    URL

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://mern-pharm-back.onrender.com',
+    baseURL: process.env.REACT_APP_API_URL,
 });
 
 instance.interceptors.request.use((config) => {
