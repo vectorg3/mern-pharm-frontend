f62d24e8ecc7d5ca3fed56287f8ba42a75c0a213
vectorg3 <diglytail@gmail.com>
2023-03-12 11:43:29 +0300

    userorders updated

--- src/components/Order/UserOrderItem.jsx
+++ src/components/Order/UserOrderItem.jsx
@@ -1,42 +1,38 @@
-import { useDispatch } from 'react-redux';
-import styles from './OrderStyles.module.scss';
+import { useDispatch } from "react-redux";
+import styles from "./OrderStyles.module.scss";
 
 function UserOrderItem(props) {
-    const { _id, address, phone, orderList, status } = props;
-    return (
-        <li className={styles.order__item}>
-            <div className={styles.cart__item_title}>Заказ {_id}</div>
-            <div>
-                {orderList.map((el) => (
-                    <li className={styles.cart__item}>
-                        <img
-                            src={el.full_background}
-                            alt=''
-                            className={styles.cart__item_img}
-                        />
-                        <div className={styles.cart__item_title}>
-                            {el.name}
-                            <p>
-                                Кол-во:
-                                <span className={styles.cart__quantity}>
-                                    {el.quantity}
-                                </span>
-                                = {el.price * el.quantity} руб.
-                            </p>
-                        </div>
-                    </li>
-                ))}
+  const { _id, address, phone, orderList, status } = props;
+  return (
+    <li className={styles.order__item}>
+      <div className={styles.cart__item_title}>Заказ {_id}</div>
+      <div className={styles.orders__list}>
+        {orderList.map((el) => (
+          <div className={styles.cart__list}>
+            <img
+              src={el.full_background}
+              alt=""
+              className={styles.cart__item_img}
+            />
+            <div className={styles.cart__item}>{el.name}</div>
+            <div className={styles.cart__item}>
+              Кол-во:
+              <span className={styles.cart__quantity}>{el.quantity}</span>={" "}
+              {el.price * el.quantity} руб.
             </div>
-            <div className={styles.cart__item_title}>
-                Адрес: <span style={{ background: '#7b68ee' }}>{address}</span>
-            </div>
-            <div className={styles.cart__item_title}>
-                Номер телефона: <span style={{ background: '#7b68ee' }}>{phone}</span>
-            </div>
-            <div className={styles.cart__item_title}>
-                Статус: <span style={{ background: '#7b68ee' }}>{status}</span>
-            </div>
-        </li>
-    );
+          </div>
+        ))}
+      </div>
+      <div className={styles.cart__item}>
+        Адрес: <span style={{ fontWeight: "bold" }}>{address}</span>
+      </div>
+      <div className={styles.cart__item}>
+        Номер телефона: <span style={{ fontWeight: "bold" }}>{phone}</span>
+      </div>
+      <div className={styles.cart__item}>
+        Статус: <span style={{ fontWeight: "bold" }}>{status}</span>
+      </div>
+    </li>
+  );
 }
 export { UserOrderItem };
